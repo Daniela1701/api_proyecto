afdf715c1c5e8e1138c2ff8d604822fcca4186c1
Daniela1701 <i202409920@cibertec.edu.pe>
2025-02-09 11:26:17 -0500

    app

--- src/app.js
+++ src/app.js
@@ -44,11 +44,5 @@ app.use("/api/clientes", clientesRoutes);
 app.use("/api/ventas", ventasRoutes);
 app.use("/api/reporte", reporteRoutes);
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en el puerto ${PORT}`);
-});
-
 
 export default app;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import app from "./app.js";
-import { PORT } from "./config.js";
+import app from './app.js';
+import { testDBConnection } from './db.js';
+
+const PORT = 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+  testDBConnection();
+});
