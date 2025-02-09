e5b6a08cf2b76cfb89397a93dcd0908ffd39edc5
Daniela1701 <i202409920@cibertec.edu.pe>
2025-02-09 08:38:44 -0500

    login

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,20 +1,19 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import { pool } from '../db.js'; // Asegúrate de importar correctamente la conexión a la DB
-import { SECRET_KEY } from '../config.js'; // Importar tu clave secreta desde el archivo config.js
+import { pool } from '../db.js'; 
+import { SECRET_KEY } from '../config.js'; 
 
 const login = async (req, res) => {
   const { username, passwword } = req.body;
 
   try {
-    // Buscar el usuario en la base de datos
+ 
     const [rows] = await pool.execute('SELECT * FROM usuarios WHERE username = ?', [username]);
 
     if (rows.length === 0) {
       return res.status(401).json({ message: 'Usuario no encontrado' });
     }
 
-    // Verificar la contraseña
     const usuarioEncontrado = rows[0];
     const esValida = await bcrypt.compare(passwword, usuarioEncontrado.passwword);
 
@@ -22,7 +21,6 @@ const login = async (req, res) => {
       return res.status(401).json({ message: 'Contraseña incorrecta' });
     }
 
-    // Generar un token JWT
     const token = jwt.sign({ id: usuarioEncontrado.id }, SECRET_KEY, { expiresIn: '1h' });
 
     return res.status(200).json({ message: 'Inicio de sesión exitoso', token });
