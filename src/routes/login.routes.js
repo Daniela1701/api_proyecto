ff8744e4ac41b970102202f79556324234a19eae
Daniela1701 <i202409920@cibertec.edu.pe>
2025-02-09 14:32:08 -0500

    login

--- src/routes/login.routes.js
+++ src/routes/login.routes.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import { login } from '../controllers/authController.js';
+import { login } from '../controllers/login.controller.js';
 
 const router = express.Router();
 
-router.post('/login', login);
+router.post('/', login);
 
 export default router;
