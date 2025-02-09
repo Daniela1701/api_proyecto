0dc34266c847e203fee6e4ff102fca3ba34de7ff
BriMelany <briyitmelanycapcha@gmail.com>
2025-02-09 10:41:29 -0500

    Cambios

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const PORT = process.env.PORT || 3000;
-export const DB_HOST = process.env.DB_HOST || "localhost";
-export const DB_USER = process.env.DB_USER || "root";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "";
-export const DB_DATABASE = process.env.DB_DATABASE || "crm_ventas";
+export const PORT = process.env.PORT || 5432;
+export const DB_HOST = process.env.DB_HOST || "dpg-cukbjt2j1k6c73d641q0-a.oregon-postgres.render.com";
+export const DB_USER = process.env.DB_USER || "db_yoga_user";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "qtK44bLby28kDq62xAPQWpVtPMj8kvVk";
+export const DB_DATABASE = process.env.DB_DATABASE || "db_yoga";
 export const SECRET_KEY = process.env.SECRET_KEY || "mi_clave_secreta";
