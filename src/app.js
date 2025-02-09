bfb90809c3f8c2d967944d9697b39c6815e7f5df
Daniela1701 <i202409920@cibertec.edu.pe>
2025-02-09 09:11:08 -0500

    ready

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use("/api/clientes", clientesRoutes);
 app.use("/api/ventas", ventasRoutes);
 app.use("/api/reporte", reporteRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
